235d9f684216f50b65c6eab6a26ad6ca6b3783cc
Jean Felipe <69043587+JeanFelippee@users.noreply.github.com>
2026-07-24 19:40:02 -0300

    Funcionalidade salvando e restarando funcionando

--- js/app.js
+++ js/app.js
@@ -3,7 +3,6 @@ const inputTarefa = document.querySelector('#novaTarefa');
 const btAdcionar = document.querySelector('.btAdicionar');
 const tarefas = document.querySelector('.tarefas');
 const dadosDasTarefas = [];
-let numero = 0;
 let numeroArray = 0;
 
 
@@ -12,10 +11,10 @@ btAdcionar.addEventListener('click', criarNovaTarefa);
 inputTarefa.addEventListener('keyup', (event) => {
     if (event.key == 'Enter') {
         criarNovaTarefa();
-    }
+    };
 });
-
-semTarefaCadastrada();
+//Função para restaurar os dados quando aba for aberta novamente!
+restarandoLocalStorage();
 
 //função que atualiza a tela
 function criarNovaTarefa() {
@@ -26,43 +25,44 @@ function criarNovaTarefa() {
         return;
     }
 
-    criandoElemento(texto);
+    //salvo o dado
     salvandoDados(texto);
     limpandoCampo();
 
-}
+};
 
 // função de validação de campo
 function validarTarefa(texto) {
     return texto !== ''
-}
+};
 
 //função que cria o elemento
-function criandoElemento(texto) {
-    const tarefa = document.createElement('li');
-    tarefa.classList.add('tarefa');
-    tarefa.setAttribute('data-key', `${numero}`);
+function criandoElemento(dadosArray) {
+    for (let i in dadosArray) {
+        const tarefa = document.createElement('li');
+        tarefa.classList.add('tarefa');
+        tarefa.setAttribute('data-key', `${dadosArray[i].id}`);
 
-    const inputCheckBox = document.createElement('input');
-    inputCheckBox.type = 'checkbox';
-    inputCheckBox.value = 'check';
+        const inputCheckBox = document.createElement('input');
+        inputCheckBox.type = 'checkbox';
+        inputCheckBox.value = 'check';
 
-    const textTarefa = document.createElement('span');
-    textTarefa.textContent = texto;
+        const textTarefa = document.createElement('span');
+        textTarefa.textContent = dadosArray[i].texto;
 
-    const btExcluir = document.createElement('button');
-    btExcluir.classList.add('btExcluir');
+        const btExcluir = document.createElement('button');
+        btExcluir.classList.add('btExcluir');
 
-    const img = document.createElement('img');
-    img.src = "assets/delete.png"
-    btExcluir.append(img);
+        const img = document.createElement('img');
+        img.src = "assets/delete.png"
+        btExcluir.append(img);
 
-    tarefa.append(inputCheckBox, textTarefa, btExcluir);
-    tarefas.append(tarefa);
+        tarefa.append(inputCheckBox, textTarefa, btExcluir);
+        tarefas.append(tarefa);
 
-    numero++;
-    clickCheck(inputCheckBox, tarefa);
-    excluirTarefa(btExcluir, tarefa)
+        clickCheck(inputCheckBox, tarefa);
+        excluirTarefa(btExcluir, tarefa)
+    };
 
 };
 
@@ -70,18 +70,21 @@ function criandoElemento(texto) {
 function limpandoCampo() {
     inputTarefa.value = '';
     inputTarefa.focus();
-}
+};
 
 //função pra verificação da tarefa
 function clickCheck(inputCheckBox, tarefa) {
     inputCheckBox.addEventListener('click', () => {
         if (inputCheckBox.checked) {
-            tarefa.querySelector('span').style.textDecoration = 'line-through'
+            tarefa.querySelector('span').style.textDecoration = 'line-through';
             tarefa.style.opacity = 0.5;
+
+
+
         } else {
             tarefa.querySelector('span').style = '';
             tarefa.style.opacity = 1;
-        }
+        };
     });
 };
 
@@ -98,7 +101,14 @@ function excluirTarefa(button, tarefa) {
 
 //função que salva os dados das mensagem em um array
 function salvandoDados(tarefa) {
-    dadosDasTarefas.push({ id: numeroArray, texto: tarefa });
+    dadosDasTarefas.push(
+        { id: numeroArray, texto: tarefa, check: false }
+    );
+    localStorage.setItem('tarefaUser', JSON.stringify(dadosDasTarefas));
+
+    let ultimoArray = dadosDasTarefas.at(-1);
+    criandoElemento([ultimoArray]);
+
     numeroArray++
     semTarefaCadastrada();
 
@@ -109,6 +119,14 @@ function excluindoDados(key) {
     let dadosArray = dadosDasTarefas.filter(item => item.id !== key);
     dadosDasTarefas.length = 0;
     dadosDasTarefas.push(...dadosArray);
+
+    let dadosStorange = JSON.parse(localStorage.getItem('tarefaUser'));
+    let storageAtualizado = dadosStorange.filter(item => item.id !== key);
+    localStorage.setItem('tarefaUser', JSON.stringify(storageAtualizado));
+
+    if (dadosDasTarefas.length > 0) {
+        numeroArray = dadosDasTarefas.at(-1).id + 1;
+    }
     semTarefaCadastrada();
 };
 
@@ -119,10 +137,23 @@ function semTarefaCadastrada() {
         let msg = document.createElement('li');
         msg.classList.add('msg');
         msg.textContent = 'Nenhuma tarefa cadastrada';
-        numero = 0;
         numeroArray = 0;
+        localStorage.clear();
         tarefas.append(msg);
     } else if (tarefas.querySelector('.msg') !== null) {
         tarefas.querySelector('.msg').remove();
     };
+};
+
+function restarandoLocalStorage() {
+    const tarefasSalvas = JSON.parse(localStorage.getItem('tarefaUser')) || [];
+    dadosDasTarefas.push(...tarefasSalvas);
+
+    //verificação para atualiza o numero do array e tarefa
+    if (dadosDasTarefas.length > 0) {
+        numeroArray = dadosDasTarefas.at(-1).id + 1;
+    };
+
+    criandoElemento(dadosDasTarefas);
+    semTarefaCadastrada();
 };
